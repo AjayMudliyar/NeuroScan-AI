813c7ba324e7ae906a19737dbde6eb8180ebe201
ajaymudliyar66@gmail.com <Ajay Mudliyar>
2025-12-21 20:17:45 +0530

    added

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 import numpy as np
 import cv2
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 from PIL import Image
 from streamlit_lottie import st_lottie
 import requests
